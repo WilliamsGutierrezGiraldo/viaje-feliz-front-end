6d7eacd9bca8b0a2f3511f8e419b3a8f885a9a97
wagutierrez <wagutierrez@modusoftware.com>
2020-08-07 19:30:03 -0500

    Se agregan imagenes de presentacion

--- src/components/Bienvenida.js
+++ src/components/Bienvenida.js
@@ -5,7 +5,8 @@ export default function Bienvenida() {
         <div className="container text-center mt-5">
             <h3>Bienvenidos a Viaje Feliz S.A</h3>
             <hr/>
-            <img src="imagenes/inicio.jpg" className="rounded mx-auto d-block" alt="Imagen de inicio"></img>
+            <img src="./imagenes/inicio.jpg" width="800" height="500"
+                className="rounded mx-auto d-block" alt="Imagen de inicio"></img>
         </div>
     )
 }
